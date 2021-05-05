40185afe42cbb2d75fbce0463227c98ec31c86c0
Dang Kien <dangvankien98@live.com>
2021-05-05 22:59:36 +0700

    Done

---
+++ src/main/java/com/kien/personapi/controller/PersonController.java
@@ -0,0 +1,41 @@
+package com.kien.personapi.controller;
+
+import com.kien.personapi.models.Person;
+import com.kien.personapi.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    PersonService personService;
+
+    @PostMapping
+    public Person createPerson(@RequestBody Person person) {
+        return personService.createPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getAll() {
+        return personService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Person getPerson(@PathVariable("id") Long id) {
+        return personService.getById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Person updatePerson(@PathVariable("id") Long id, @RequestBody Person person) {
+        return personService.updatePerson(id, person);
+    }
+
+    @DeleteMapping("/{id}")
+    public Person deletePerson(@PathVariable("id") Long id) {
+        return personService.deletePerson(id);
+    }
+}
--- src/main/java/com/kien/personapi/models/Person.java
+++ src/main/java/com/kien/personapi/models/Person.java
@@ -16,4 +16,6 @@ public class Person {
     private String fullName;
 
     private int age;
+
+    private boolean alive;
 }
